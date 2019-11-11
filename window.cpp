b8a471a1f93faad4c010e6646dca8c266a1a3f7b
ahmedhussien91 <a.hussien.91@gmail.com>
2019-11-11 10:21:34 +0200

    Add files via upload

--- window.cpp
+++ window.cpp
@@ -1,54 +1,193 @@
 #include "window.h"
 #include <math.h>       /* log */
 
-void window::rearrange_array(int16_t* arr_1D) { 
-	//block_size = 64 (8x8)
-	//image_width = 512 
-	// create 64 x arr_Blocks_2D_size containers for 1d arr
-	uint32_t block_size = m_win_conf->block_hight*m_win_conf->block_width;
-	uint32_t Image_width = m_image_o->pixels_width;
-	image_o_num_of_blocks = ((m_image_o->pixels_hight*m_image_o->pixels_width) / block_size);
-	image_o_blocks = new int16_t * [image_o_num_of_blocks];
-	for (uint32_t i = 0; i < image_o_num_of_blocks; i++) {
-		image_o_blocks[i] = new int16_t[block_size];
-	}
+// void window::rearrange_array(int16_t* arr_1D) { 
+// 	//block_size = 64 (8x8)
+// 	//image_width = 512 
+// 	// create 64 x arr_Blocks_2D_size containers for 1d arr
+// 	uint32_t block_size = m_win_conf->block_hight*m_win_conf->block_width;
+// 	uint32_t Image_width = m_image_o->pixels_width;
+// 	image_o_num_of_blocks = ((m_image_o->pixels_hight*m_image_o->pixels_width) / block_size);
+// 	image_o_blocks = new int16_t * [image_o_num_of_blocks];
+// 	for (uint32_t i = 0; i < image_o_num_of_blocks; i++) {
+// 		image_o_blocks[i] = new int16_t[block_size];
+// 	}
 
-	// fill cotainer with 2d data
-	uint32_t b_h = m_win_conf->block_hight, b_w = m_win_conf->block_width;
-	uint32_t image_h = m_image_o->pixels_hight;
-	uint32_t col_idx = -1, hight_offset = 0;
-	hight_offset = (b_h * Image_width);
-	for (uint32_t h = 0; h < image_h / b_h; h++) {
-		for (uint32_t i = 0; i < Image_width / b_w; i++) {
-			col_idx = -1;
-			for (uint32_t j = 0; j < block_size; j++) {
+// 	// fill cotainer with 2d data
+// 	uint32_t b_h = m_win_conf->block_hight, b_w = m_win_conf->block_width;
+// 	uint32_t image_h = m_image_o->pixels_hight;
+// 	uint32_t col_idx = -1, hight_offset = 0;
+// 	hight_offset = (b_h * Image_width);
+// 	for (uint32_t h = 0; h < image_h / b_h; h++) {
+// 		for (uint32_t i = 0; i < Image_width / b_w; i++) {
+// 			col_idx = -1;
+// 			for (uint32_t j = 0; j < block_size; j++) {
 
-				if (j % b_w == 0)
-				{
-					col_idx++;
+// 				if (j % b_w == 0)
+// 				{
+// 					col_idx++;
+// 				}
+// 				image_o_blocks[i + (h * (Image_width / b_w))][j] = arr_1D[(j % b_w) + (col_idx * Image_width) + (i * b_w) + hight_offset * h];
+// 			}
+// 		}
+// 	}
+// }
+
+window::window(image* image_o, image* next_image, window_conf_t* win_conf) {
+	m_image_o = image_o;
+	m_next_image = next_image;
+	m_win_conf = win_conf;
+	
+	//rearrange_array(m_image_o->YUV_image_in.Y_buff); // fill image_o_blocks, image_o_num_of_blocks
+	
+	/* calculate num_of_windows */
+	// block change every 1 pixel - (block_hight-1)*windows_width
+	no_of_pixels_win = (m_win_conf->window_hight *m_win_conf->window_width)-((m_win_conf->block_hight-1) * m_win_conf->window_width) ; 
+	no_of_windows_in_width = (m_next_image->pixels_width / m_win_conf->window_width) + (2*(uint32_t)(m_win_conf->window_width/2));
+	no_of_windows_in_hight = m_next_image->pixels_hight / m_win_conf->window_hight + (2 * (uint32_t)(m_win_conf->window_hight / 2));
+	no_of_windows = no_of_windows_in_hight * no_of_windows_in_hight;
+	no_of_blocks_win_width = m_win_conf->window_width / m_win_conf->block_width;
+	no_of_blocks_win_hight = m_win_conf->window_hight / m_win_conf->block_hight;
+	no_of_blocks_win = no_of_blocks_win_width * no_of_blocks_win_hight;
+	block_size = m_win_conf->block_hight * m_win_conf->block_width;
+	
+	int16_t* middle_block = new int16_t [block_size];
+	int16_t* other_block = new int16_t [block_size];
+	motion_vector.block_motion_vector_array = new block_motion_vector_t[no_of_windows];
+	motion_vector.number_of_vectors = no_of_windows;
+
+	/* loop over windows in the m_next_image*/
+	uint32_t i = 0;
+	uint32_t middle_index= 0;
+	uint32_t win_index =0;
+	for (i = 0; i < no_of_windows; i++)
+	{
+		/*get windows index (anchor top-left)*/
+		win_index = get_win_index(i);
+		/*get the middle block id - to access in image_o_blocks*/
+		middle_index = get_middle_block_index(win_index);
+		if (middle_index == 0)
+		{ // wrong index not found 
+			_ASSERT(false);
+		}
+
+		/*loop on blocks in the window - blocks move 1 pixel*/
+		uint32_t j = 0;
+		float_t Error =0;
+		float_t min_Error = 0xFFFFFFFF;
+		motion_vector.block_motion_vector_array[i].original_block_index = middle_index;
+		motion_vector.block_motion_vector_array[i].windows_index = win_index;
+		motion_vector.block_motion_vector_array[i].distance = 0xFFFF;
+		for (j = 0; j < no_of_pixels_win;j++)
+		{
+			/*get blocks given pixel index of top left array*/
+			get_block(win_index,middle_block,m_image_o);
+			get_block(win_index,other_block,m_next_image);
+
+			/*compare the two arrays using MAD_Calc or MSE_Calc or PSNR_Calc*/
+			switch (win_conf->matching_type)
+			{
+			case MAD:
+				Error = MAD_Calc(middle_block,other_block);
+				break;
+			case PSNR:
+				Error = PSNR_Calc(middle_block,other_block);
+				break;
+
+			default:
+				_ASSERT(false);
+				break;
+			}
+			/*update motion vector for this block depending on output */
+			if(Error<min_Error) {
+				/* calculate x, y components from j and window size */
+				motion_vector.block_motion_vector_array[i].block_index = get_block_index(win_index, j);
+				motion_vector.block_motion_vector_array[i].distance  = get_distance_between_blocks(j, no_of_pixels_win/2);
+			}
+			else if(Error=min_Error) {
+				uint32_t distance;
+				/* calculate distance to middle block */
+				distance = get_distance_between_blocks(j, no_of_pixels_win/2);
+				if (distance < motion_vector.block_motion_vector_array[i].distance){
+					motion_vector.block_motion_vector_array[i].block_index = get_block_index(win_index, j);
+					motion_vector.block_motion_vector_array[i].distance  = distance;
 				}
-				image_o_blocks[i + (h * (Image_width / b_w))][j] = arr_1D[(j % b_w) + (col_idx * Image_width) + (i * b_w) + hight_offset * h];
 			}
 		}
 	}
+
+};
+
+
+uint32_t window::get_win_index(uint32_t window_index) {
+	uint32_t index = 0;
+
+	index = (window_index % no_of_windows_in_width) * m_win_conf->window_width + 
+	((window_index - (window_index % no_of_windows_in_width))/no_of_windows_in_width) *
+		m_win_conf->window_hight *m_next_image->pixels_width;
+
+	return index;
 }
 
-uint32_t window::MAD_Calc(int16_t* image_block, int16_t* next_image_block) {
+uint32_t window::get_middle_block_index(uint32_t window_index) {
+	uint32_t index = 0;
+	uint32_t middle_h_off, middle_w_off;
+
+	middle_w_off = no_of_blocks_win_width/2;
+	middle_h_off = no_of_blocks_win_hight/2;
+	index = window_index + (middle_w_off*m_win_conf->block_width) + 
+	(middle_h_off * m_win_conf->block_hight * m_next_image->pixels_width);
+	return index;
+}
+uint32_t window::get_block_index(uint32_t window_index, uint32_t index_inside_win) {
+	uint32_t index = 0;
+
+	index = window_index + (index_inside_win%m_win_conf->window_width) +
+		((index_inside_win-(index_inside_win%m_win_conf->window_width)/m_win_conf->window_width) * m_next_image->pixels_width);
+	return index;
+}
+
+void window::get_block(uint32_t offset, int16_t * block, image* image_input) {
+	uint32_t index = 0;
+	uint32_t i;
+	uint32_t block_col = -1;
+	for (i=0; i<block_size; i++)
+	{
+		if (i%block_size){
+			block_col++; 
+		}
+		index = offset + (i%block_size) + (block_col*image_input->pixels_width);
+		block[i] = image_input->YUV_image_in.Y_buff[index];
+	}
+}
+
+uint32_t window::get_distance_between_blocks(uint32_t index_1,uint32_t index_2){
+	int32_t x1, x2, y1, y2;
+	uint32_t distance;
+	x1 = index_1%m_win_conf->window_width;
+	y1 = ((index_1 - index_1%m_win_conf->window_width)/m_win_conf->window_width); 
+	x2 = index_2%m_win_conf->window_width;
+	y2 = ((index_2 - index_2%m_win_conf->window_width)/m_win_conf->window_width);
+	distance = (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2);
+	return distance;
+}
+
+float_t window::MAD_Calc(int16_t* image_block, int16_t* next_image_block) {
 	uint32_t block_size = m_win_conf->block_hight * m_win_conf->block_width;
 	uint32_t i, sum=0;
-	uint32_t MAD;
+	float_t MAD;
 	for (i = 0; i < block_size; i++)
 	{
 		sum += abs(image_block[i] - next_image_block[i]);
 	}
-	 MAD = (1 / block_size) * sum;
-	 return MAD;
+		MAD = (1 / block_size) * sum;
+		return MAD;
 }
 
-uint32_t window::MSE_Calc(int16_t* image_block, int16_t* next_image_block) {
+float_t window::MSE_Calc(int16_t* image_block, int16_t* next_image_block) {
 	uint32_t block_size = m_win_conf->block_hight * m_win_conf->block_width;
 	uint32_t i, sum = 0;
-	uint32_t MSE;
+	float_t MSE;
 	for (i = 0; i < block_size; i++)
 	{
 		sum += (image_block[i] - next_image_block[i])* (image_block[i] - next_image_block[i]);
@@ -57,8 +196,8 @@ uint32_t window::MSE_Calc(int16_t* image_block, int16_t* next_image_block) {
 	return MSE;
 }
 
-uint32_t window::PSNR_Calc(int16_t* image_block, int16_t* next_image_block) {
-	uint32_t MSE;
+float_t window::PSNR_Calc(int16_t* image_block, int16_t* next_image_block) {
+	float_t MSE;
 	MSE = MSE_Calc(image_block, next_image_block);
 	return (10 * log10((255 * 255) / MSE));
 }
--- window.h
+++ window.h
@@ -1,8 +1,15 @@
 #pragma once
 #include"image.h"
 
+typedef struct block_motion_vector_t{
+	uint32_t original_block_index;
+	uint32_t block_index;
+	uint32_t windows_index;
+	uint32_t distance;
+};
 typedef struct motion_vector_t {
-
+	block_motion_vector_t * block_motion_vector_array;
+	uint32_t number_of_vectors; 
 };
 
 typedef struct window_conf_t {
@@ -10,6 +17,7 @@ typedef struct window_conf_t {
 	uint16_t block_hight;
 	uint16_t window_width; // multiple of block width
 	uint16_t window_hight; // multiple of block hight 
+	matching_type_e matching_type;
 
 };
 
@@ -21,82 +29,16 @@ class window
 {
 public:
 
-	window(image* image_o, image* next_image, window_conf_t* win_conf) {
-		m_image_o = image_o;
-		m_next_image = next_image;
-		m_win_conf = win_conf;
-		
-		rearrange_array(m_image_o->YUV_image_in.Y_buff); // fill image_o_blocks, image_o_num_of_blocks
-		
-		/* calculate num_of_windows */
-		no_of_pixels_win = (m_win_conf->window_hight) *(m_win_conf->window_width); // block change every 1 pixel
-		no_of_windows_in_width = (m_next_image->pixels_width / m_win_conf->window_width) + (2*(uint32_t)(m_win_conf->window_width/2));
-		no_of_windows_in_hight = m_next_image->pixels_hight / m_win_conf->window_hight + (2 * (uint32_t)(m_win_conf->window_hight / 2));
-		no_of_windows = no_of_windows_in_hight * no_of_windows_in_hight;
-		no_of_blocks_win_width = m_win_conf->window_width / m_win_conf->block_width;
-		no_of_blocks_win_hight = m_win_conf->window_hight / m_win_conf->block_hight;
-		no_of_blocks_win = no_of_blocks_win_width * no_of_blocks_win_hight;
-		block_size = m_win_conf->block_hight * m_win_conf->block_width;
-		
-		/* loop over windows in the m_next_image*/
-		uint32_t i = 0;
-		uint32_t middle_index= 0;
-		for (i = 0; i < no_of_windows; i++)
-		{
-			/*get windows index (anchor top-left)*/
-			uint32_t win_index = get_win_index(i);
-			/*get the middle block id - to access in image_o_blocks*/
-			middle_index = get_middle_block_index(win_index);
-			if (middle_index == 0)
-			{ // wrong index not found 
-				_ASSERT(false);
-			}
-
-			/*loop on blocks in the window - blocks move 1 pixel*/
-			uint32_t j = 0;
-			for (j = 0; j < no_of_pixels_win;j++)
-			{
-				/*get block given pixel index of top left array*/
-
-				/*compare the two arrays using MAD_Calc or MSE_Calc or PSNR_Calc*/
-
-				/*update motion vector for this block depending on output */
-			}
-		}
-
-	};
-
-
-	uint32_t get_win_index(uint32_t window_index) {
-		uint32_t index = 0;
-
-		index = (window_index % no_of_windows_in_width) * m_win_conf->window_width + (window_index - (window_index % no_of_windows_in_width)) * m_win_conf->window_hight;
-
-		return index;
-	}
-
-	uint32_t get_middle_block_index(uint32_t window_index) {
-		uint32_t index = 0;
-		uint32_t middle_h_off, middle_w_off;
-
-		middle_w_off = no_of_blocks_win_width/2;
-		middle_h_off = no_of_blocks_win_hight/2;
-		index = window_index + middle_w_off*m_win_conf->block_width + middle_h_off * m_win_conf->block_hight * m_next_image->pixels_width;
-		return index;
-	}
-
-	void get_block(uint32_t offset, int16_t * block) {
-		uint32_t index = 0;
-		uint32_t i;
-		for (i=0; i<block_size; i++)
-		{
-
-		}
-	}
-	void rearrange_array(int16_t* arr_1D);
-	uint32_t MAD_Calc(int16_t* image_block, int16_t* next_image_block);
-	uint32_t MSE_Calc(int16_t* image_block, int16_t* next_image_block);
-	uint32_t PSNR_Calc(int16_t* image_block, int16_t* next_image_block);
+	window(image* image_o, image* next_image, window_conf_t* win_conf);
+	uint32_t get_win_index(uint32_t window_index);
+	uint32_t get_middle_block_index(uint32_t window_index);
+	uint32_t get_block_index(uint32_t window_index, uint32_t index_inside_win);
+	void get_block(uint32_t offset, int16_t * block, image* image_input);
+	uint32_t get_distance_between_blocks(uint32_t index_1,uint32_t index_2);
+	// void rearrange_array(int16_t* arr_1D);
+	float_t MAD_Calc(int16_t* image_block, int16_t* next_image_block);
+	float_t MSE_Calc(int16_t* image_block, int16_t* next_image_block);
+	float_t PSNR_Calc(int16_t* image_block, int16_t* next_image_block);
 
 	motion_vector_t vector;
 
@@ -104,7 +46,7 @@ private:
 	int16_t** image_o_blocks;
 	uint32_t image_o_num_of_blocks;
 	uint32_t no_of_windows;
-	uint32_t no_of_pixels_win;
+	uint32_t no_of_pixels_win; // number of pixels that can anchor a block in a window
 	uint32_t no_of_windows_in_width;
 	uint32_t no_of_windows_in_hight;
 	uint32_t no_of_blocks_win_width;
@@ -114,5 +56,6 @@ private:
 	window_conf_t* m_win_conf;
 	image* m_image_o;
 	image* m_next_image;
+	motion_vector_t motion_vector;
 };
 
